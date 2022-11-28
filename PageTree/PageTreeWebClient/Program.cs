0a628f40021eb72962138df3e2dabfb88bd2e51b
dariusz.lusnia <dariusz.lusnia@gmail.com>
2022-11-28 10:17:21 +0100

    Move project folders

---
+++ PageTree/PageTreeWebClient/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using PageTreeWebClient;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("BlazorWASMHostedMSAL.ServerAPI",
+    client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+builder.Services.AddScoped(
+    sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorWASMHostedMSAL.ServerAPI"));
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes
+        .Add("https://pagetree.onmicrosoft.com/32b11564-4bac-4a95-b8eb-0bdccefd99db/access_as_user");
+});
+
+await builder.Build().RunAsync();
