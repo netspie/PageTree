66d642c178035b9f5880b8b628920068a27dbfad
dariusz.lusnia <dariusz.lusnia@gmail.com>
2022-12-22 21:46:17 +0100

    Fix errors

--- PageTree/PageTree.Client.Shared/Services/PagesService.cs
+++ PageTree/PageTree.Client.Shared/Services/PagesService.cs
@@ -9,33 +9,33 @@ namespace PageTree.Client.Shared.Services
 
     }
 
-    internal class UsersService : BaseService
-    {
-        private readonly IDataService _dataService;
+    //internal class UsersService : BaseService
+    //{
+    //    private readonly IDataService _dataService;
 
-        public Task CreateUser()
-        {
+    //    public Task CreateUser()
+    //    {
 
-        }
-    }
+    //    }
+    //}
 
-    internal abstract class BaseService
-    {
-        private readonly IHttpClientFactory _clientFactory;
-        private readonly ISignInRedirector _signInRedirector;
+    //internal abstract class BaseService
+    //{
+    //    private readonly IHttpClientFactory _clientFactory;
+    //    private readonly ISignInRedirector _signInRedirector;
 
-        public BaseService(IHttpClientFactory clientFactory, ISignInRedirector signInRedirector)
-        {
-            _clientFactory = clientFactory;
-            _signInRedirector = signInRedirector;
-        }
+    //    public BaseService(IHttpClientFactory clientFactory, ISignInRedirector signInRedirector)
+    //    {
+    //        _clientFactory = clientFactory;
+    //        _signInRedirector = signInRedirector;
+    //    }
 
-        protected Task<TResponse> GetResource<TResponse>(IQuery<Result<TResponse>> query, string resourcePath, CancellationToken ct = default)
-        {
-            var queryString = query.GetQueryString();
-            var resourcePathWithQuery = $"{resourcePath}?{queryString}";
+    //    protected Task<TResponse> GetResource<TResponse>(IQuery<Result<TResponse>> query, string resourcePath, CancellationToken ct = default)
+    //    {
+    //        var queryString = query.GetQueryString();
+    //        var resourcePathWithQuery = $"{resourcePath}?{queryString}";
 
-            return _clientFactory.GetResource<TResponse> (_signInRedirector, resourcePathWithQuery, ct);
-        }
-    }
+    //        return _clientFactory.GetResource<TResponse, > (_signInRedirector, resourcePathWithQuery, ct);
+    //    }
+    //}
 }
