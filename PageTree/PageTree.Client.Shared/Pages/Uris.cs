a92a7590a91c3c95a20b6c47841581855c71e5a6
dariusz.lusnia <dariusz.lusnia@gmail.com>
2023-01-01 01:41:04 +0100

    Add public uris

---
+++ PageTree/PageTree.Client.Shared/Pages/Uris.cs
@@ -0,0 +1,10 @@
+﻿namespace PageTree.Client.Shared.Pages
+{
+    public static class Uris
+    {
+        public const string Home = "/pageTree";
+        public const string Learn = Home + "/learn";
+        public const string Projects = Learn + "/projects";
+        public const string ProjectEdit = Learn + "/projects/edit";
+    }
+}
