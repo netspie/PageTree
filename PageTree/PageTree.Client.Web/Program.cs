20126c13ba97a032f35f6a4fce56e6f0feef0cdb
dariusz.lusnia <dariusz.lusnia@gmail.com>
2023-01-04 19:25:22 +0100

    Fix navbar and sidemenu + login redirection

--- PageTree/PageTree.Client.Web/Program.cs
+++ PageTree/PageTree.Client.Web/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
     options.ProviderOptions.Cache.CacheLocation = "localStorage";
+    options.AuthenticationPaths.LogOutCallbackPath = "/";
     options.ProviderOptions.DefaultAccessTokenScopes
         .Add("https://pagetree.onmicrosoft.com/32b11564-4bac-4a95-b8eb-0bdccefd99db/access_as_user");
 });
