f8e97176a4dfc8a9400cd4bf89fdf21815523543
dariusz.lusnia <dariusz.lusnia@gmail.com>
2023-01-06 19:28:48 +0100

    Fix menu and login

---
+++ PageTree/PageTree.Client.Shared/Interfaces/RefreshInvoker.cs
@@ -0,0 +1,15 @@
+﻿namespace PageTree.Client.Shared.Interfaces
+{
+    public class RefreshInvoker
+    {
+        public Func<Task> Action { set; private get; }
+
+        public Task Invoke()
+        {
+            if (Action != null)
+                return Action.Invoke();
+
+            return Task.CompletedTask;
+        }
+    }
+}
