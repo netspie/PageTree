a1a18d417c161349bc628cf92cab5e52862ada11
dariusz.lusnia <dariusz.lusnia@gmail.com>
2023-03-22 14:36:02 +0100

    Add controller for practice tactics

---
+++ PageTree/PageTree.Server/Controllers/PracticeTacticsController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Corelibs.AspNetApi.Authorization;
+using Corelibs.AspNetApi.Controllers.Extensions;
+using Corelibs.AspNetApi.ModelBinders;
+using Mediator;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PageTree.App.UseCases.PracticeCategories.Commands;
+using PageTree.Server.ApiContracts;
+
+namespace PageTree.Server.Controllers
+{
+    namespace PageTree.Server.Api.Controllers
+    {
+        [ApiController]
+        [Route("api/v1/practiceTactics")]
+        [Authorize]
+        public class PracticeTacticsController : ControllerBase
+        {
+            private readonly IMediator _mediator;
+            private readonly IMapper _mapper;
+
+            public PracticeTacticsController(IMediator mediator, IMapper mapper)
+            {
+                _mediator = mediator;
+                _mapper = mapper;
+            }
+
+            [HttpPost, Authorize_Edit]
+            public Task<IActionResult> Create([FromRouteAndBody] CreatePracticeTacticApiCommand command = null) =>
+                _mediator.MapSendAndGetPostResponse<CreatePracticeTacticCommand>(command, _mapper);
+
+            [HttpDelete, Route("{practiceTacticID}"), Authorize_Edit]
+            public Task<IActionResult> Delete([FromRouteAndBody] DeletePracticeTacticApiCommand command) =>
+                _mediator.MapSendAndGetDeleteResponse<DeletePracticeTacticCommand>(command, _mapper);
+
+            [HttpPatch, Route("{practiceTacticID}/changeName"), Authorize_Edit]
+            public Task<IActionResult> ChangeName([FromRouteAndBody] ChangeNameOfPracticeTacticApiCommand command) =>
+                _mediator.MapSendAndGetPatchResponse<ChangeNameOfPracticeTacticCommand>(command, _mapper);
+
+            [HttpPatch, Route("{practiceTacticID}/changeIndex"), Authorize_Edit]
+            public Task<IActionResult> ChangeIndex([FromRouteAndBody] ChangeIndexOfPracticeTacticApiCommand command) =>
+                _mediator.MapSendAndGetPatchResponse<ChangeIndexOfPracticeTacticCommand>(command, _mapper);
+        }
+    }
+}
---
+++ PageTree/PageTree.Server/Controllers/PracticeTacticsOfProjectController.cs
@@ -0,0 +1,32 @@
+using AutoMapper;
+using Corelibs.AspNetApi.Controllers.Extensions;
+using Corelibs.AspNetApi.ModelBinders;
+using Mediator;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PageTree.App.UseCases.PracticeCategories.Queries;
+using PageTree.Server.ApiContracts;
+
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace PageTree.Server.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/projects/{projectID}/practiceTactics")]
+    [Authorize]
+    public class PracticeTacticsOfProjectController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public PracticeTacticsOfProjectController(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        [HttpGet, AllowAnonymous]
+        public Task<IActionResult> Get([FromRouteAndQuery] GetProjectPracticeTacticsApiQuery query) =>
+            _mediator.MapSendAndGetResponse<GetProjectPracticeTacticsQuery, GetProjectPracticeTacticsQueryOut>(query, _mapper);
+    }
+}
